e61d12fbf3b2aa877591b29728cd702e4ae7a918
EOrtega27 <ortegachaveznsm@gmail.com>
2022-12-31 21:27:21 -0500

    message from application.properties

--- src/main/java/ed/example/oc/thymeleafDemo/HomeController.java
+++ src/main/java/ed/example/oc/thymeleafDemo/HomeController.java
@@ -1,6 +1,7 @@
 package ed.example.oc.thymeleafDemo;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +9,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @Slf4j
 public class HomeController {
+    @Value("${index.mensaje}")
+    private String saludo;
     @GetMapping("/")
     public String inicio(Model model){
         String mensaje = "Hola mundo con Thymeleaf";
         model.addAttribute("mensaje", mensaje);
+        model.addAttribute("saludo", saludo);
         log.info("Controlador Spring MVC");
         return "index";
     }
